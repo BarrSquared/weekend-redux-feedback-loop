35a95c430ec38eabdf0e12861951c450b782f2c4
Russell Barr <rbarrsquared@gmail.com>
2021-09-26 13:09:53 -0500

    Page1Feeling populated with form, useState value for feeling, and dispatch

--- src/components/Page1Feeling/Page1Feeling.jsx
+++ src/components/Page1Feeling/Page1Feeling.jsx
@@ -1,12 +1,44 @@
 import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Page1Feeling() {
 
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-    return(
+    let [feeling, setFeeling] = useState('');
+
+    const submitFeeling = (event) => {
+        event.preventDefault();
+        console.log('Updating feeling rating.');
+        if (feeling >= 1 && feeling <= 5) {
+            dispatch({
+                type: 'SET_FEELING_RATING',
+                payload: feeling
+            });
+            clearFeeling();
+        } else {
+            alert("You must enter a number between 1 and 5");
+            clearFeeling();
+            return;
+        }
+    }
+    const clearFeeling = () => {
+        setFeeling('');
+    }
+
+
+    return (
         <section>
             <h2>How are you feeling today?</h2>
+            <form onSubmit={submitFeeling} >
+                <input
+                    onChange={(event) => setFeeling(event.target.value)}
+                    value={feeling}
+                    type="number"
+                    placeholder="1-5"
+                />
+            </form>
         </section>
     );
 }
