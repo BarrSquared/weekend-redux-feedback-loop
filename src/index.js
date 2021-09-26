0d8be7afd6bf69c4e6bee7e911ce40a5de2d0ef8
Russell Barr <rbarrsquared@gmail.com>
2021-09-26 13:16:59 -0500

    imported react-redux functions to index.js, created defaultReview object, reviewToAdd reducer, and reduxStore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleWare } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultReview = {feeling: '', understanding: '', support: '', comments: ''};
+
+const reviewToAdd = (state = defaultReview, action) => {
+    if(action.type === 'SET_FEELING_RATING') {
+        return {...state, feeling: action.payload.feeling};
+    }
+    return state;
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        reviewToAdd,
+    }),
+    applyMiddleWare(logger)
+);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
